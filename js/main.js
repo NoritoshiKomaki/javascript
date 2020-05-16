3c29a205481a471d8cb2a09b9d6a839da5bbe86d
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-05-16 12:17:56 +0900

    lesson2

--- js/main.js
+++ js/main.js
@@ -1,87 +1,62 @@
-console.log('%c [JavaScript]thisについて学ぼう', 'color:red; font-size: 1.5em');
+console.log('%c [JavaScript]call,apply,bindの使い方', 'color:red; font-size: 1.5em');
 
-// this = myObj
+// call関数で引数を渡すことでthisに代入される
+function greet(){
+  let hi = `Hi, ${this.name}`;
+  console.log(hi);
+}
+let obj = {name: 'Tom'};
+greet.call(obj);
+
+// callの第二引数以降はargumentsに格納される
+// sliceを使用することで配列で取得可能
+function greet2(){
+  let slicedArray = [].slice.call(arguments,0,1);
+  console.log(slicedArray);
+}
+greet2.call(obj,1,2,3);
+
+// applyは第二引数に関数を渡す
+function greet3(id1, id2, id3){
+  console.log(id1, id2, id3)
+  let hello = `Hello, ${this.name}`
+  console.log(hello);
+}
+greet3.apply(obj,[1,2,3]);
+
+// minで最小値を取得する際に使用できる
+const arry = [1,2,3,4];
+console.log(
+  Math.min.apply(null, arry)
+);
+
+// setTimeoutの構造
+// window {
+//   setTimeout: function(callBack, milliSecond){
+//     stop milliSecond
+//     callBack();
+//   }
+// }
+
+// setTimeoutはwindowに格納されている
+// bindを使用することでthis = window→myObjに変更
 let myObj = {
-  id: 2,
-  printId(){
-    console.log(this.id);
+  id: 42,
+  print() {
+    setTimeout(function () {
+      console.log(this.id);
+    }.bind(this), 1000);
   }
 }
+myObj.print();
 
-myObj.printId();
-
-const sayFoo = function(){
-  console.log(this['foo']);
-}
-
-// グローバル変数(windowに格納される)
-foo = 'foo';
-
-const mySecondObj = {
-  foo: "I'm in the obj",
-  sayFoo
-}
-
-// 実行される場所によってthisの内容が変わる
-sayFoo()
-mySecondObj.sayFoo()
-
-// instance化されたオブジェクトを参照
-function MyObj(id){
-  this.id = id;
-}
-
-MyObj.prototype.printId = function(id) {
-  console.log(this.id);
-}
-
-const newInstance = new MyObj(5);
-
-class MyClass {
-  constructor(id){
-    this.id = id;
-  }
-  
-  printId(id) {
-    console.log(this.id);
+// アロー関数を使用するとwindowは無視される
+let myObj2 = {
+  id: 42,
+  print() {
+    setTimeout( () => {
+      console.log(this.id);
+    }, 1000);
   }
 }
-
-// const newInstance = new MyObj(5);
-
-// func1: this = func1
-// func2: this = window
-// func3: this = window
-const outer = {
-  func1: function(){
-    console.log(this);
-
-    const func2 = function() {
-      console.log(this);
-
-      const func3 = function(){
-        console.log(this);
-      }();
-    }();
-  }
-}
-
-outer.func1();
-
-// thisを変数に代入することで全てfunc1
-const outer2 = {
-  func1: function(){
-    console.log(this);
-    let _that = this;
-
-    const func2 = function() {
-      console.log(_that);
-
-      const func3 = function(){
-        console.log(_that);
-      }();
-    }();
-  }
-}
-
-outer2.func1();
+myObj2.print();
