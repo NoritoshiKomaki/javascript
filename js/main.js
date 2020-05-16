9e579c2598667f4ed334e355a1e362af65ad8581
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-05-16 18:24:48 +0900

    lesson4

--- js/main.js
+++ js/main.js
@@ -1,59 +1,48 @@
-console.log('%c [JavaScript]アロー関数について学ぼう', 'color:red; font-size: 1.5em');
+console.log('%c [JavaScript]クロージャーについて学ぼう', 'color:red; font-size: 1.5em');
 
-// 通常の関数定義
-function timesTwo(i){
-  return i * 2;
-}
-const res = timesTwo(2);
-console.log(res);
+let counter = 0;
+increment(); //1
+increment(); //2
+increment(); //3
 
-// 関数を定数で定義する
-const timesTwo2 = function(i){
-  return i * 2;
-}
-const res2 = timesTwo2(2);
-console.log(res2)
-
-// アロー関数(1行の場合は{}とreturnを省略可能)
-const timesTwo3 = i => i * 2;
-const res3 = timesTwo3(2);
-console.log(res3)
-
-// 書き方(シンタックス)について
-let arrowFn;
-arrowFn = () => 42;
-arrowFn = x => 42;
-arrowFn = (x) => 42;
-arrowFn = (x, y) => 42;
-arrowFn = (x, y) => {
-  console.log(x + y);
-  return x + y;
+// ダメなパターン
+// counterが別の箇所で変更される恐れがある
+function increment() {
+  counter++;
+  console.log(counter);
 }
 
-// thisのバインドについて
-let normalFn;
-normalFn = {
-  id: 43,
-  counter: function() {
-    console.log(this.id);
+// 即時関数
+let increment2 = (function () {
+  let counter = 0;
+  return function () {
+    counter++;
+    console.log(counter);
+  }
+})();
+increment2();
+increment2();
+increment2();
 
-    window.setTimeout(() => {
-      console.log(this.id);
-    }, 1000);
+// 変数定義
+let increment3 = function countUp(i) {
+  let counter = i;
+    counter++;
+    console.log(counter);
+};
+increment3(0);
+increment3(1);
+increment3(2);
+
+// tailにaddStringFactoryの引数が代入される
+function addStringFactory(tail) {
+// strにaddAsの引数が代入される
+  return function(str) {
+    return str + tail;
   }
 };
-normalFn.counter();
-
-// globalが出力される
-window.me = 'global';
-const outer = function(){
-  let me = 'outer';
-  return {
-    me: 'inner',
-// functionにするとinnerが出力される
-    say: () => {
-      console.log(this.me);
-    }
-  }
-}
-outer().say();
+let addAs = addStringFactory('AAAAAA');
+let addBs = addStringFactory('BBBBBB');
+let str2 = 'Tom';
+str3 = addAs(str2);
+console.log(str3)
