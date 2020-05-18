3bb55830cf85e9a553c9f2a1078e6b67d01bf581
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-05-18 15:27:24 +0900

    lesson16

--- js/main.js
+++ js/main.js
@@ -1,29 +1,25 @@
-console.log('%c [JavaScript]オブジェクトとJavaScript3', 'color:red; font-size: 1.5em');
+console.log('%c [JavaScript]オブジェクトとJavaScript4', 'color:red; font-size: 1.5em');
 
 function Person(first, last){
   this.first = first;
   this.last = last;
-  // this.introduce = function(){
-  //   console.log('My name is ' + first + ' ' + last);
-  // }
-}
-
-// prototypeではthisを記述する必要がある
-Person.prototype.introduce = function(){
-  console.log('My name is ' + this.first + ' ' + this.last);
 }
 
 let me = new Person('First', 'Last');
-let me2 = new Person('Me2', 'Desu');
-// meのみ関数を書き換えることができる
-// me.introduce = function() {
-//   console.log("I don't want to introduce myself");
-// }
 
-// 関数をまとめて書き換えることができる
-// インスタンス化すると__proto__と記述する
-me.__proto__.introduce = function() {
-  console.log("I don't want to introduce myself");
+// 1.直代入の関数が最優先される
+me.introduce = function(){
+  console.log('My name is ' + this.first + ' ' + this.last);
 }
+
+// 2.コンストラクタprototypeが二番目に優先される
+Person.prototype.introduce = function(){
+  console.log('Person ' + this.first + ' ' + this.last);
+}
+
+// 3.オブジェクトprototypeが三番目に優先される
+Object.prototype.introduce = function(){
+  console.log('Object ' + this.first + ' ' + this.last);
+}
+
 me.introduce();
-me2.introduce();
