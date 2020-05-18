dbc4113cf6317b56443bad347397e9d01323a117
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-05-18 15:14:52 +0900

    lesson15

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
-console.log('%c [JavaScript]オブジェクトとJavaScript2', 'color:red; font-size: 1.5em');
+console.log('%c [JavaScript]オブジェクトとJavaScript3', 'color:red; font-size: 1.5em');
 
-function factoryPerson(first, last){
-  let person = {first, last};
-  return person;
-}
-let me = factoryPerson('Komaki', 'Noritoshi');
-console.log(me);
-
-// コンストラクター関数は頭文字を大文字にする
 function Person(first, last){
   this.first = first;
   this.last = last;
+  // this.introduce = function(){
+  //   console.log('My name is ' + first + ' ' + last);
+  // }
 }
-let me2 = new Person('小牧', '功知')
-console.log(me2);
+
+// prototypeではthisを記述する必要がある
+Person.prototype.introduce = function(){
+  console.log('My name is ' + this.first + ' ' + this.last);
+}
+
+let me = new Person('First', 'Last');
+let me2 = new Person('Me2', 'Desu');
+// meのみ関数を書き換えることができる
+// me.introduce = function() {
+//   console.log("I don't want to introduce myself");
+// }
+
+// 関数をまとめて書き換えることができる
+// インスタンス化すると__proto__と記述する
+me.__proto__.introduce = function() {
+  console.log("I don't want to introduce myself");
+}
+me.introduce();
+me2.introduce();
