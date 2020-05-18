982b39b2b1cad2e06eb0a2c26d674a093e9ef5e1
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-05-18 14:56:52 +0900

    lesson14

--- js/main.js
+++ js/main.js
@@ -1,28 +1,16 @@
-console.log('%c [JavaScript]オブジェクトとJavaScript', 'color:red; font-size: 1.5em');
+console.log('%c [JavaScript]オブジェクトとJavaScript2', 'color:red; font-size: 1.5em');
 
-// オブジェクトにはprototypeが格納されている
-let obj = {};
-console.log(obj);
+function factoryPerson(first, last){
+  let person = {first, last};
+  return person;
+}
+let me = factoryPerson('Komaki', 'Noritoshi');
+console.log(me);
 
-let arry = new Array();
-console.log(arry);
-
-obj.name = 'Tom';
-console.log(obj);
-
-// オブジェクトに配列を入れることができる
-obj.arry = ['1', 1];
-console.log(obj);
-
-// オブジェクトにオブジェクトを入れることができる
-obj.obj = {name: 'John'};
-console.log(obj);
-
-// Tomが出力される
-console.log(obj.name);
-console.log(obj['name']);
-let name = 'obj';
-// johnが出力される
-console.log(obj[name]);
-// Tomが出力される
-console.log(obj.name);
+// コンストラクター関数は頭文字を大文字にする
+function Person(first, last){
+  this.first = first;
+  this.last = last;
+}
+let me2 = new Person('小牧', '功知')
+console.log(me2);
